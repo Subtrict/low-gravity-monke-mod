adfd9777ec284efa84f55958ec4d899195deb224
Christopher <weinhardtchristopher@gmail.com>
2021-06-13 14:33:39 -0400

    update to 1.0.8

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -6,9 +6,9 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "LowGravityMonke_v0.1.0.qmod"
-$TempArchiveName = "LowGravityMonke_v0.1.0.qmod.zip"
+$ArchiveName = "LowGravityMonke_v0.2.1.qmod"
+$TempArchiveName = "LowGravityMonke_v0.2.1.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libLowGravityMonke.so", "./libs/arm64-v8a/libbeatsaber-hook_1_3_5.so", "./mod.json" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libLowGravityMonke.so", "./libs/arm64-v8a/libbeatsaber-hook_1_3_5.so", "./mod.json", "./coverImage.png" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
